1ff523011cc3cacdf8686cc908f3bfc628fc2510
Goktug Varan <g.varan@hotmail.com>
2026-01-08 16:25:52 +0300

    feat: home screen with navigation buttons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,28 @@
-import {View, Text} from 'react-native';
+import {View, Button, StyleSheet} from 'react-native';
 
-export default function HomeScreen() {
+export default function HomeScreen({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      {/* +++ Navigate to Camera Screen */}
+      <Button 
+        title="Camera & Gallery" 
+        onPress={() => navigation.navigate('Camera')} 
+      />
+      
+      {/* +++ Navigate to Location Screen */}
+      <Button 
+        title="Location" 
+        onPress={() => navigation.navigate('Location')} 
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 16,
+    justifyContent: 'center',
+    padding: 20,
+  },
+});
